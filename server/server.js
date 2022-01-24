7600dbb7e437c08b31044f4ae49e11b69e181aac
Robert Isaacs <robertisaacs87@yahoo.com>
2022-01-23 20:17:06 -0600

    debugging server

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === 'production') {
 
 // app.use(routes);
 
-//get all
+// //get all
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
